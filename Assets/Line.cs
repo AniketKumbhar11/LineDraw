df801d9584ecbc5e99e48dbd480460338f961498
AniketKumbhar11 <atkumbhar1@gmail.com>
2021-04-27 11:17:28 +0530

    first commit

---
+++ Assets/Line.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public LineRenderer linerenderer;
+    public EdgeCollider2D edgecollider2d;
+
+    List<Vector2> points;
+
+    private void Start()
+    {
+
+        edgecollider2d = GetComponent<EdgeCollider2D>();
+    }
+    public void UpdateLine(Vector2 mousepos)
+    {
+        if (points==null)
+        {
+            points = new List<Vector2>();
+            setPoints(mousepos);
+            return;
+        }
+        if(Vector2.Distance(points.Last(),mousepos)>0.1f)
+        {
+            setPoints(mousepos);
+        }
+    }
+
+    public void setPoints(Vector2 point)
+    {
+        points.Add(point);
+        linerenderer.positionCount = points.Count;
+        linerenderer.SetPosition(points.Count - 1, point);
+        if (points.Count>1)
+ 
+        edgecollider2d .points = points.ToArray();
+    }
+}
---
+++ Assets/Linecreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linecreator : MonoBehaviour
+{
+    public GameObject LinePrefab;
+    Line ActiveLine;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           GameObject LinePre= Instantiate(LinePrefab);
+           ActiveLine= LinePre.GetComponent<Line>();
+        }
+        if (ActiveLine!=null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            ActiveLine.UpdateLine(mousePos);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            ActiveLine = null; 
+        }
+    }
+}
